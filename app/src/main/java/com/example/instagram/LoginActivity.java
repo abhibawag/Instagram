4480875b078c34b3bd3399ba27e50ca9b4d0c102
abhibawa <bawa3052000@gmail.com>
2020-04-21 00:36:18 +0530

    Sign up the user and login logout the user

---
+++ app/src/main/java/com/example/instagram/LoginActivity.java
@@ -0,0 +1,62 @@
+package com.example.instagram;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.sdsmdg.tastytoast.TastyToast;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText edtEmailLogin, edtPasswordLogin;
+    private Button btnLogin, btnPreviousActivity;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        setTitle("Log In");
+        edtEmailLogin = findViewById(R.id.edtEmailLogin);
+        edtPasswordLogin = findViewById(R.id.edtPasswordLogin);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnPreviousActivity = findViewById(R.id.btnPreviousActivity);
+
+
+        //Log in the user
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                ParseUser.logInInBackground(edtEmailLogin.getText().toString(), edtPasswordLogin.getText().toString(), new LogInCallback() {
+                    @Override
+                    public void done(ParseUser user, ParseException e) {
+                        if(user!=null && e==null){
+                            TastyToast.makeText(LoginActivity.this, user.getUsername().toString() + " Logged In Successfully!", TastyToast.LENGTH_SHORT, TastyToast.SUCCESS).show();
+                        }else{
+                            TastyToast.makeText(LoginActivity.this, e.getMessage(), TastyToast.LENGTH_SHORT, TastyToast.ERROR).show();
+                        }
+                    }
+                });
+
+            }
+        });
+
+
+        //Go to previous activity i.e Sign Up page
+        btnPreviousActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/instagram/MainActivity.java
@@ -0,0 +1,107 @@
+package com.example.instagram;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.strictmode.IntentReceiverLeakedViolation;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+import com.sdsmdg.tastytoast.TastyToast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText edtEmailSignup, edtPasswordSignup, edtUsernameSignup;
+    private Button btnSignup, btnNextActivity;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setTitle("Sign Up");    // set title bar text to sign up
+        edtEmailSignup = findViewById(R.id.edtEmailSignup);
+        edtPasswordSignup = findViewById(R.id.edtPasswordSignup);
+        edtUsernameSignup = findViewById(R.id.edtUsernameSignup);
+        btnSignup = findViewById(R.id.btnSignup);
+        btnNextActivity = findViewById(R.id.btnNextActivity);
+
+
+        //Log out currently logged user
+        if(ParseUser.getCurrentUser() != null){
+            String currentUser = ParseUser.getCurrentUser().getUsername().toString()+"";
+            ParseUser.getCurrentUser().logOut();
+            TastyToast.makeText(MainActivity.this, currentUser + " Logged Out", TastyToast.LENGTH_SHORT, TastyToast.DEFAULT).show();
+
+        }
+
+
+
+        //Pressing enter key will sign up the user
+        edtPasswordSignup.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+
+                if(keyCode == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_DOWN){
+                    btnSignup.isPressed();
+                }
+                return false;
+            }
+        });
+
+
+
+
+        //Sign up a new user
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(edtEmailSignup.getText().toString().equals("") || edtPasswordSignup.getText().toString().equals("") || edtUsernameSignup.getText().toString().equals("")){
+
+                    TastyToast.makeText(MainActivity.this, "Email/Username/Password cannot be empty!", TastyToast.LENGTH_SHORT, TastyToast.INFO).show();
+
+                }else{
+
+                final ParseUser appUser = new ParseUser();
+                appUser.setEmail(edtEmailSignup.getText().toString());
+                appUser.setUsername(edtUsernameSignup.getText().toString());
+                appUser.setPassword(edtPasswordSignup.getText().toString());
+                final ProgressDialog progressDialog = new ProgressDialog(MainActivity.this);
+                progressDialog.setMessage(appUser.getUsername().toString() + " Signing Up");
+                progressDialog.show();
+                appUser.signUpInBackground(new SignUpCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if(e==null){
+
+                            TastyToast.makeText(getApplicationContext(), appUser.getUsername().toString()+" Signed Up Successfully!", TastyToast.LENGTH_LONG, TastyToast.SUCCESS).show();
+
+                        }
+                        progressDialog.dismiss();
+
+                    }
+                });
+            }
+        }});
+
+
+        //Move to next activity i.e login page
+        btnNextActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+}
